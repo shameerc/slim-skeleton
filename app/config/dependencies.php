3675dafabc0908f191fb51ed61e7f649a0e3dceb
shameerc <shameer806@gmail.com>
2016-01-29 22:00:15 +0400

    Update dependencies to work with latest aura di. Thanks to @pmjones :)

--- app/config/dependencies.php
+++ app/config/dependencies.php
@@ -11,7 +11,6 @@ $di->set('view', function () use ($di) {
     // Add extensions
     $view->addExtension(new Slim\Views\TwigExtension($di->get('router'), $di->get('request')->getUri()));
     $view->addExtension(new Twig_Extension_Debug());
-
     return $view;
 });
 
@@ -25,24 +24,34 @@ $di->set('callableResolver', new App\CallableResolver($di));
 // Service factories 
 // -----------------------------------------------------------------------------
 // monolog
+// $di->params['Monolog\Logger']['name'] = $settings['logger']['name'];
 $di->params['Monolog\Logger']['name'] = $settings['logger']['name'];
-$logger = $di->newInstance('Monolog\Logger');
-$logger->pushProcessor(new \Monolog\Processor\UidProcessor());
-$logger->pushHandler(new \Monolog\Handler\StreamHandler($settings['logger']['path'], \Monolog\Logger::DEBUG));
-$di->set('logger', $logger);
+$di->set('logger', function () use ($di) {
+    $settings = $di->get('settings');
+    $logger = $di->newInstance('Monolog\Logger');
+    $logger->pushProcessor(new \Monolog\Processor\UidProcessor());
+    $logger->pushHandler(new \Monolog\Handler\StreamHandler($settings['logger']['path'], \Monolog\Logger::DEBUG));
+    return $logger;
+});
 
 // Lazyloading doctrine
-$settings = $di->get('settings');
-$config = $settings['database'];
-$di->set('db', $di->lazy(['\Doctrine\DBAL\DriverManager', 'getConnection'], $config, $di->lazyNew('\Doctrine\DBAL\Configuration')));
+$di->set('settings:database', function () use ($di) {
+    $settings = $di->get('settings');
+    return $settings['database'];
+});
+$di->set('db', $di->lazy(
+    ['\Doctrine\DBAL\DriverManager', 'getConnection'],
+    $di->lazyGet('settings:database'),
+    $di->lazyNew('\Doctrine\DBAL\Configuration')
+));
 
 // Model Injections
 $di->params['App\Model\AbstractModel']['db'] = $di->lazyGet('db');
 
 // Controller Injections
 // Inject view and logger using setter injection
-$di->setters['App\Controller\AbstractController']['setView'] = $di->get('view');
-$di->setters['App\Controller\AbstractController']['setLogger'] = $di->get('logger');
+$di->setters['App\Controller\AbstractController']['setView'] = $di->lazyGet('view');
+$di->setters['App\Controller\AbstractController']['setLogger'] = $di->lazyGet('logger');
 
 // Simply pass a usermodel object 
 // @TODO, Pass Services to controller instead of Models
